f44d4eb95b7ceff2202eaae5bba7514035a12ce7
kovarsky <boris.kovarsky@gmail.com>
2014-01-13 18:49:30 +0400

    GettingSharedKmers

--- code/GetSharedKmers.py
+++ code/GetSharedKmers.py
@@ -1,5 +1,17 @@
 import sys
 
+complements = {"A": "T",
+                "G": "C",
+                "T": "A",
+                "C": "G"}
+
+def get_complement(sequence):
+    r_complement = ''
+    for symb in sequence[::-1]:
+        r_complement += complements[symb]
+    return(r_complement)
+
+
 
 def fetch_kmers(seq, kmer_len):
     seq_len = len(seq)
@@ -11,16 +23,18 @@ def fetch_kmers(seq, kmer_len):
 
 def main():
     with open(sys.argv[1]) as f:
+        kmer_len = int(f.readline().strip())
         seq_1 = f.readline().strip()
         seq_2 = f.readline().strip()
 
     shared_pairs = []
     for i, kmer_1 in enumerate(fetch_kmers(seq_1, kmer_len)):
         for j, kmer_2 in enumerate(fetch_kmers(seq_2, kmer_len)):
-            if kmer_1 == kmer_2 or kmer_1 == kmer_2[::-1]:
+            if kmer_1 == kmer_2 or kmer_1 == get_complement(kmer_2):
+                print (i, j)
                 shared_pairs.append((i, j))
 
-    print shared_pairs
+    # print shared_pairs
 
 if __name__ == '__main__':
     main()
