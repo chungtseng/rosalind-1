14657030b655630703c9f12e062003b6cad90fe3
kovarsky <boris.kovarsky@gmail.com>
2014-01-14 20:20:10 +0400

    Kmer matching problem

--- code/GetSharedKmers.py
+++ code/GetSharedKmers.py
@@ -1,4 +1,7 @@
 import sys
+from collections import defaultdict
+from itertools import product
+import re
 
 complements = {"A": "T",
                 "G": "C",
@@ -21,20 +24,56 @@ def fetch_kmers(seq, kmer_len):
         i += 1
         yield subseq
 
+def get_kmer_dict(kmer_iter, complement=True):
+    kmer_dict = defaultdict(list)
+    for i, kmer in enumerate(kmer_iter):
+        kmer_dict[kmer].append(i)
+        if complement:
+            kmer_dict[get_complement(kmer)].append(i)
+    return kmer_dict
+
 def main():
     with open(sys.argv[1]) as f:
         kmer_len = int(f.readline().strip())
         seq_1 = f.readline().strip()
         seq_2 = f.readline().strip()
 
-    shared_pairs = []
-    for i, kmer_1 in enumerate(fetch_kmers(seq_1, kmer_len)):
-        for j, kmer_2 in enumerate(fetch_kmers(seq_2, kmer_len)):
-            if kmer_1 == kmer_2 or kmer_1 == get_complement(kmer_2):
-                print (i, j)
-                shared_pairs.append((i, j))
+    kmer_dict_1 = get_kmer_dict(fetch_kmers(seq_1, kmer_len), complement=False)
+    kmer_dict_2 = get_kmer_dict(fetch_kmers(seq_2, kmer_len))
 
-    # print shared_pairs
+    sorted_keys_1 = set(kmer_dict_1.keys())
+    sorted_keys_2 = set(kmer_dict_2.keys())
+
+    shared_pos = []
+
+    for kmer in sorted_keys_1 & sorted_keys_2:
+        for pos in product(kmer_dict_1[kmer],kmer_dict_2[kmer]):
+            shared_pos.append(pos)
+
+    # for kmer in sorted_keys_1 & comp_sorted_keys_2:
+    #     comp = get_complement(kmer)
+    #     for pos in product(kmer_dict_1[kmer],kmer_dict_2[comp]):
+    #         shared_pos.append(pos)
+
+    for p in sorted(shared_pos):
+        print p
+
+    # for kmer, coord in kmer_dict_1.items():
+    #     if kmer in kmer_dict_2.keys():
+    #         
+    #     elif get_complement(kmer) in kmer_dict_2.keys():
+    #         comp = get_complement(kmer)
+    #         for pos in product(kmer_dict_1[kmer],kmer_dict_2[comp]):
+    #             print pos
+    #     else:
+    #         pass
+
+    # for kmer in fetch_kmers(seq_1,kmer_len):
+    #     s1 = re.findall(kmer, seq_2)
+    #     s2 = re.findall(get_complement(kmer), seq_2)
+    #     if s1 or s2:
+    #         print s1 + s2
+
 
 if __name__ == '__main__':
     main()
