694c019cae908c796bd033ec962c31aa4983c7f8
kovarsky <boris.kovarsky@gmail.com>
2014-01-21 12:02:24 +0400

    LSRP failed

--- code/LongestSharedRepeatProblem.py
+++ code/LongestSharedRepeatProblem.py
@@ -2,6 +2,7 @@ import sys
 from itertools import izip
 from copy import copy
 from itertools import chain
+from copy import deepcopy
 
 def get_lcp_array(suff_array, text):
     lcp_array = []
@@ -38,6 +39,7 @@ class Tree():
         
     def add(self, pattern, signature):
         l = 0
+        # print signature
         self.signature.add(signature)
         for child in self.children:
             l = 0
@@ -49,15 +51,17 @@ class Tree():
             if l:
                 if len(child.data ) == l:
                     child.add(pattern[l:], signature)
-                    # child.signature.add(signature)
+                    child.signature.add(signature)
                 else:
                     child_copy = copy(child)
+                    child_copy.data = child_copy.data[l:]
+                    child_copy.parent = child
+                    child_copy.signature = deepcopy(child.signature)
+
 
                     child.children = [child_copy, Tree(pattern[l:], signature, child)]
                     child.data = pattern[:l]
                     child.signature.add(signature)
-
-                    child_copy.data = child_copy.data[l:]
                 break
         if not l:
             self.children.append(Tree(pattern, signature,self))
@@ -103,9 +107,11 @@ def get_max_shared_substr(tree):
     for subtree in tree.children:
         if len(subtree.signature) > 1:
             seq = tree.data + get_max_shared_substr(subtree)
+            print seq
             shared_seqs.append(seq)
         else:
             pass
+    print shared_seqs
     return max(shared_seqs)
 
 
@@ -119,18 +125,20 @@ def main():
         # lcp_array_1 = get_lcp_array(suff_array_1, seq1)
 
     iter1 = ((seq1[i:], 1) for i in range(len(seq1)))
-    iter2 = ((seq2[i:], 2) for i in range(len(seq2)))
+    iter2 = ((seq2[j:], 2) for j in range(len(seq2)))
 
     # for suff_pos, lcp in zip(suff_array_2, lcp_array_2):
     #     tree.construct(seq2[suff_pos:], lcp, 2)
 
     for pattern, signature in chain(iter1, iter2):
+    # for pattern, signature in iter1:
         # print pattern, signature
         tree.add(pattern, signature)
     
+    for subtree in tree.children:
+        print subtree.data, subtree.signature
 
-
-    # bypass_tree(tree)
+    bypass_tree(tree)
     print get_max_shared_substr(tree)
 
 
